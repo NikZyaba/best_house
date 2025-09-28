aecd135b60eaee2f3551b39894bd8a94751fee2c
NikZyaba <nikitazjabko@rambler.ru>
2025-09-28 10:12:24 +0300

    main:consultingform.html(css), django-prj:urls.py

--- best_house_prg/urls.py
+++ best_house_prg/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("apps.main.urls", namespace="main")),
+    path("", include("apps.main.urls")),
     path("", include("apps.user.urls", namespace="user")),
     path('admin/', admin.site.urls),
     
